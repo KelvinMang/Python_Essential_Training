d987114ffe218c53421f8c0e7e9883cfa4477ade
Kelvin Mang <42444869+KelvinMang@users.noreply.github.com>
2022-06-30 13:04:03 +0100

    Rainbow

--- Part 1/Now_in_termcolor.py
+++ Part 1/Now_in_termcolor.py
@@ -2,15 +2,9 @@ import sys
 import termcolor
 from termcolor import colored, cprint
 
-text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
-print(text)
-cprint('Hello, World!', 'green', 'on_red')
+color = ["red", "yellow", "green", "cyan", "blue", "magenta"]
+i = 0 
+for range in color:
+    cprint("-----Rainbow-----",color[i])
+    i = i+1
 
-print_red_on_cyan = lambda x: cprint(x, 'red', 'on_cyan')
-print_red_on_cyan('Hello, World!')
-print_red_on_cyan('Hello, Universe!')
-
-for i in range(10):
-    cprint(i, 'magenta', end=' ')
-
-cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
