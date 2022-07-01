06cce0c90de25dbcde8f9e421381380edb983164
Kelvin Mang <42444869+KelvinMang@users.noreply.github.com>
2022-07-01 14:50:20 +0100

    1/7/2022

--- 1.Gearing_up_for_Python/Now_in_termcolor.py
+++ 1.Gearing_up_for_Python/Now_in_termcolor.py
@@ -3,8 +3,6 @@ import termcolor
 from termcolor import colored, cprint
 
 color = ["red", "yellow", "green", "cyan", "blue", "magenta"]
-i = 0 
 for range in color:
-    cprint("-----Rainbow-----",color[i])
-    i = i+1
+    cprint("-----Rainbow-----", range)
 
